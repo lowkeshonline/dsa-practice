30a556ddbce9b6cf3f492ae74114f74db3eee2a2
lowkeshonline <lowkeshonline@gmail.com>
2024-11-30 19:59:39 +0530

    Added comments

--- Patterns/halfDiamond.cpp
+++ Patterns/halfDiamond.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 void halfDiamondTriangle(int n){
+    //Loop for the outer lines
     for(int i = 1; i <= 2 * n - 1; i++){
+        //Printing stars acc to i until it reaches N value
         int star = i;
+        //If i exceeds N value update star value in reverse order
         if(i > n){
             star = 2 * n -i;
         }
+        //Loop for printing star values.
         for(int j = 1; j <= star; j++){
             cout << "*";
         }
@@ -15,5 +19,7 @@ void halfDiamondTriangle(int n){
 }
 
 int main(){
-    halfDiamondTriangle(5);
+    int n;
+    cin >> n;
+    halfDiamondTriangle(n);
 }
