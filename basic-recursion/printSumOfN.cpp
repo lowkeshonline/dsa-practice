0a985cec53d8b1a5bd02cfe9b85392e508b4ee84
lowkeshonline <lowkeshonline@gmail.com>
2025-02-08 12:15:33 +0530

    updated parameterised and functional method

--- basic-recursion/printSumOfN.cpp
+++ basic-recursion/printSumOfN.cpp
@@ -3,18 +3,28 @@ using namespace std;
 
 //Print the sum of N integers without using Recursion (Parameterised)
 
-int printSumOfN(int n , int sum){
+int parameterSumOfN(int n , int sum){
     if(n < 1){
-        cout << sum;
+        cout << sum << endl;
         return sum;
     }
-    printSumOfN(n-1 , sum + n);
+    parameterSumOfN(n-1 , sum + n);
+}
+
+//print the sum of N integers without using Recursion (Functional)
+
+int funcSumOfN(int n){
+    if(n == 0){
+        return 0;
+    }
+    return n + funcSumOfN(n -1);
 }
 
 
 int main(){
     int n;
     cin >> n;
-    int res = printSumOfN(n , 0);
+    parameterSumOfN(n , 0);
+    cout << funcSumOfN(n) << endl;
     return 0;
 }
