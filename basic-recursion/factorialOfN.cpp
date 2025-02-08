e30a50311231ef12a15fcd069a27672f7e9325e7
lowkeshonline <lowkeshonline@gmail.com>
2025-02-08 12:37:28 +0530

    updated parameterised and functional methods

--- basic-recursion/factorialOfN.cpp
+++ basic-recursion/factorialOfN.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 //Print factorial of N using Recursion(Parameterised method)
 
-int paramFactOfN(int n , int fact){
+int paramFactOfN(int n , long long fact){
     if(n < 1){
         cout << fact << endl;
         return fact;
@@ -11,11 +11,20 @@ int paramFactOfN(int n , int fact){
     paramFactOfN(n - 1, fact * n);
 }
 
+//Print factorial of N using Recursion (Functional Method)
+
+int funcFactOfN(long long n){
+    if(n == 0){
+        return 1;
+    }
+    return n * funcFactOfN(n-1);
+}
+
 
 int main(){
-    int n;
+    long long n;
     cin >> n;
     paramFactOfN(n , 1);
-    
+    cout << funcFactOfN(n) << endl;
     return 0;
 }
