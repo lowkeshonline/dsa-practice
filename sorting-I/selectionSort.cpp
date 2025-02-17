83b757d33c4ff5a9cc67e676cf720431f74ee532
lowkeshonline <lowkeshonline@gmail.com>
2025-02-17 22:31:11 +0530

    selectionSort Revision

--- sorting-I/selectionSort.cpp
+++ sorting-I/selectionSort.cpp
@@ -1,32 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void selection_sort(int arr[], int n) {
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
-      }
+void selectionSort(int arr[] , int n){
+    //Outer loop goes till less than last element
+    for(int i = 0; i < n-1; i++){
+        //consider the [i] as the least for now
+        int min = i;
+        //Compare it with each element that comes after [i]
+        for(int j = i+1; j < n; j++){
+            //Update the min value to j if any element is found to be smaller than i
+            if(arr[i] > arr[j]){
+                min = j;
+            }    
+        }
+        //Swap the min and i variables
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
-  }
-
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
+    // Print the sorted array after the loop exections are done
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
 }
-int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  selection_sort(arr, n);
-  return 0;
+
+int main(){
+    //Initialize and array input
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n ; i++){
+        cin >> arr[i];
+    }
+    //Funtion call
+    selectionSort(arr , n);
 }
