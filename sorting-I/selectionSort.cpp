7ba76c946838bc94228bd3c0b15d3c2177f0deb6
lowkeshonline <lowkeshonline@gmail.com>
2025-02-23 10:17:08 +0530

    selectionSort revision

--- sorting-I/selectionSort.cpp
+++ sorting-I/selectionSort.cpp
@@ -1,37 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int arr[] , int n){
-    //Outer loop goes till less than last element
-    for(int i = 0; i < n-1; i++){
-        //consider the [i] as the least for now
+//Selection sort revision
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<=n-1; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<=n-1; i++){
         int min = i;
-        //Compare it with each element that comes after [i]
-        for(int j = i+1; j < n; j++){
-            //Update the min value to j if any element is found to be smaller than i
-            if(arr[i] > arr[j]){
+        for(int j = i+1; j<=n-1; j++){
+            if(arr[j] < arr[min]){
                 min = j;
-            }    
+            }
         }
-        //Swap the min and i variables
         int temp = arr[i];
         arr[i] = arr[min];
         arr[min] = temp;
     }
-    // Print the sorted array after the loop exections are done
-    for(int i = 0; i < n; i++){
+
+    for(int i=0; i<=n-1; i++){
         cout << arr[i] << " ";
     }
-}
-
-int main(){
-    //Initialize and array input
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i = 0; i < n ; i++){
-        cin >> arr[i];
-    }
-    //Funtion call
-    selectionSort(arr , n);
 }
