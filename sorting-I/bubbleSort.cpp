f8efdb6fd975a095e713dd5d68390f68b0a5bf27
lowkeshonline <lowkeshonline@gmail.com>
2025-02-23 10:50:46 +0530

    bubble Sort revison

--- sorting-I/bubbleSort.cpp
+++ sorting-I/bubbleSort.cpp
@@ -1,36 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[], int n){
-    // Outer loop goes from last index (n-1) to first index (0)
-    for(int i = n-1; i>=0; i--){
-        //Inner loop goes from first index to one element less than i value (i-1) to avoid runtime error
-        for(int j = 0; j<= i-1; j++){
-            //if arr[j] (first element) > (next element) 
-            //swap
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j]; 
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
-
+//Bubble sort revision
 
 int main(){
-    //Initialize an array input
-    int n;
-    cin >> n;
+    int n = 5;
     int arr[n];
-    for(int i = 0; i < n; i++){
+    for(int i=0; i<= n-1; i++){
         cin >> arr[i];
     }
-    //Function call
-    bubbleSort(arr,n);
-    //Print the array
-    for(int i = 0; i<n; i++){
+
+    for(int i = 0; i<=n-1; i++){
+        bool didSwap = 0;
+        for(int j = 0; j<= n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                didSwap = 1;
+            }
+        }
+        if(didSwap == false){
+            break;
+        }
+    }
+
+    for(int i = 0; i <= n-1; i++){
         cout << arr[i] << " ";
     }
-    
 }
