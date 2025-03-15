1c0d81314ab1185a78ddf49eeff36f0eb3f28a69
lowkeshonline <lowkeshonline@gmail.com>
2025-03-15 23:54:57 +0530

    second largest in array

---
+++ Arrays/secondLargest.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //creating an array
+    int arr[] = {92, 44, 93, 72, 13, 1, 34};
+
+    //storing the length of the array
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    //sorting the array
+    sort (arr, arr+n);
+    
+    //finding the largest element
+    int largest = arr[n-1];
+
+    //finding the second largest
+    int secondLargest = -1;
+    for(int i = n-2; i >= 0; i++){
+        if(arr[i] != largest){
+            secondLargest = arr[i];
+            break;
+        }
+    }
+
+    //Sorted array
+    cout << "Sorted array : ";
+    for(int num : arr){
+        cout << num << " ";
+    }
+    cout << "\n";
+    
+    //Largest and second largest numbers
+    cout << "Largest element : " << largest << "\n";
+    if(secondLargest == -1){
+        cout << "All the elements in array are same";
+    } else {
+        cout << "Second Largest element : " << secondLargest << "\n";
+    }
+
+}
