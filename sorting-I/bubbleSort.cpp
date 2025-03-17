24b350c03fb461d8c3d81b805e8246e562478ed4
lowkeshonline <lowkeshonline@gmail.com>
2025-03-17 08:12:15 +0530

    bubble sort rev

--- sorting-I/bubbleSort.cpp
+++ sorting-I/bubbleSort.cpp
@@ -1,32 +1,40 @@
-#include <bits/stdc++.h>
+//Bubble Sort is the most simplest algorithm among all the sorting algorithm it works by repeatedly swapping the adjacent elements if they are in the wrong order. Bubble sort algorithm is not useful for large data sets since it's average and worst time complexity are quite high
+
+//Given Array : {33, 12, 89, 41, 9, 62, 28, 85, 24, 34, 91}
+
+// Bubble sort Implementation
+// Time Complexity : O(n^2)
+// Space complexity : O(1)
+
+#include  <bits/stdc++.h>
 using namespace std;
 
-//Bubble sort revision
-
 int main(){
-    int n = 5;
-    int arr[n];
-    for(int i=0; i<= n-1; i++){
-        cin >> arr[i];
-    }
-
-    for(int i = 0; i<=n-1; i++){
-        bool didSwap = 0;
-        for(int j = 0; j<= n-i-1; j++){
+    //Creating an array
+    int arr[] = {12 ,9 ,24 ,28 ,33 ,34 ,41 ,62 ,85 ,89 ,91};
+    //Storing size of array in (n) variable
+    int n = sizeof(arr) / sizeof(arr[0]);
+    //Bubble sort Implementation
+    for(int i = 0; i < n-1; i++){
+        //swap check
+        int isSwapped = false;
+        for(int j = 0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 //swap
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                didSwap = 1;
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                isSwapped = true;
             }
         }
-        if(didSwap == false){
+        //if no item is swapped in the inner loop then end sorting
+        if(isSwapped == false){
             break;
         }
     }
-
-    for(int i = 0; i <= n-1; i++){
-        cout << arr[i] << " ";
+    //Print the array
+    for(int num : arr){
+        cout << num << " ";
     }
+
 }
