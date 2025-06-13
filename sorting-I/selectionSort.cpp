a0a79e8d07f9a76282998b4601bcc9da8234ee97
lowkeshonline <lowkeshonline@gmail.com>
2025-06-13 17:45:43 +0530

    insertion sort

--- sorting-I/selectionSort.cpp
+++ sorting-I/selectionSort.cpp
@@ -1,31 +1,29 @@
+// -- Insertion sort ---
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-//Selection sort : It is a comparison based sorting algorithm. It is divided into two parts 1) Sorted 2) Unsorted. It repeatedly selects the smallest element from the unsorted array and shift it to the last position of the sorted array
-
-//Time complexity : O(n^2)
-//Space complexity : O(1)
-
-int main(){
-    //Create an array
-    int arr[] = {58, 90, 41, 78, 33, 21, 82};
-    //Storing the array size into a variable
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    for(int i = 0; i<= n-2; i++){
+void insertionSort (int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         int min = i;
-        for(int j = i; j <= n-1; j++){
-            if(arr[min] > arr[j]){
+        for (int j = i+1; j < size; j++) {
+            if (arr[j] < arr[min]) {
                 min = j;
             }
         }
-        //swap
-        int temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
+        swap(arr[min] , arr[i]);
     }
-    //Print the array
-    for(int num : arr){
-        cout << num << " ";
+}
+
+int main() {
+    int arr[] = { 34, 2, 90, 23, 89, 42, 60};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    insertionSort(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
 }
