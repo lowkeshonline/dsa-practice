731b5d2c658c3b437440e9bef35cb2434bd27b75
lowkeshonline <lowkeshonline@gmail.com>
2025-06-14 06:19:43 +0530

    insertion sort

--- sorting-I/insertionSort.cpp
+++ sorting-I/insertionSort.cpp
@@ -1,26 +1,29 @@
+// --- Insertion sort --- 
+
 #include <bits/stdc++.h>
 using namespace std;
 
-//Insertion sort revision
 
-int main(){
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i=0; i<=n-1; i++){
-        cin >> arr[i];
-    }
-
-    for(int i=1; i<=n-1; i++){
-        while(arr[i] < arr[i-1] && i > 0){
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
-            i--;
+void insertionSort (int arr[], int n) {
+    
+    for (int i = 0; i < n; i++) {
+        int j = i;
+        while (j >= 0 && arr[j] < arr[j - 1]) {
+            swap(arr[j] , arr[j - 1]);
+            j--;
         }
     }
 
-    for(int i=0; i<=n-1; i++){
+}
+
+int main() {
+    int arr[] = { 84, 22, 91, 2, 43, 23};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insertionSort(arr,n);
+
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
 }
