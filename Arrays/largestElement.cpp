e38e312179824aaa0385b03285ca3b9154829449
lowkeshonline <lowkeshonline@gmail.com>
2025-06-26 07:49:33 +0530

    largest_in_array

--- Arrays/largestElement.cpp
+++ Arrays/largestElement.cpp
@@ -1,23 +1,52 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[] , int size) {
+    for ( int i = 0; i < size; i++ ){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
 
+// Largest element in an array
 
-// [2 , 44, 90, 12, 98, 21]
+// Sorting method
 
-int main(){
+void findLargestBySort (int arr[], int size) {
 
-    int arr[5] = { 2 , 90, 12, 98, 21};
+    sort(arr, arr+size);
 
-    int largest = 0;
+    cout << "The largest element of the array is (By sort method) : " << arr[size - 1] << "\n";
+}
+ 
+//Recursive method
 
-    for(int i = 1; i< 5; i++){
-        if(arr[i] > arr[largest]){
-            largest = i;
+void findLargestRecursive (int arr[] , int size) {
+    int max = 0;
+
+    for (int i = 1; i < size; i++) {
+        if(arr[i] > max){
+            max = i;
         }
     }
 
-    cout << arr[largest] << "\n";
-
-    return 0;
+    cout << "The largest element in the array (using recursive method) is : " << arr[max] << "\n";
 }
+
+int main() {
+    
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    findLargestBySort(arr,size);
+    findLargestRecursive(arr,size);
+
+}
+
+
